fbaa2eb4121d19805504c604481787265b956197
Jay Patel <jay.patel1@gameskraft.com>
2025-03-21 11:49:46 +0530

    fixes

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,7 +6,9 @@ const HomePage: React.FC = () => {
   return (
     <Container className="p-2 fade-in-up h-full flex items-center justify-center flex-col">
       <div className="flex items-center justify-center flex-col bg-[#000] w-[100px] h-[100px] text-white p-2 rounded-full">
-        <p><i className="pi pi-th-large text-5xl"></i></p>
+        <p>
+          <i className="pi pi-th-large text-5xl"></i>
+        </p>
       </div>
       <MenuList />
     </Container>
